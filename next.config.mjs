fb543e1d59f9f269a6bdbfa127b475a01d271842
Александр Рукавишников <as.rukavishnikov@mail.ru>
2024-05-31 02:15:55 +0500

    images

--- next.config.mjs
+++ next.config.mjs
@@ -4,14 +4,14 @@ const nextConfig = {
         remotePatterns: [
             {
                 protocol: "https",
-                hostname: "booking-restaurants-v3va.vercel.app",
-                pathname: "/media/images/**"
+                hostname: process.env.API_IMAGE_NAME,
+                port: '',
             }
         ]
     },
     env: {
         API_URL: process.env.API_URL,
-        API_NAME: process.env.API_NAME,
+        API_IMAGE_NAME: process.env.API_IMAGE_NAME,
         NEXTAUTH_URL: process.env.NEXTAUTH_URL,
         NEXTAUTH_URL_INTERNAL: process.env.NEXTAUTH_URL_INTERNAL,
         NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
